498a2080873495774ecfdf41e9c8704f6a2db0b3
unknown <shreyanshdubey669@gmail.com>
2020-11-01 14:12:38 +0530

    Flask_Webapp 1

---
+++ Application/README.md
@@ -0,0 +1,3 @@
+# CRUD-Flask
+Hi
+hello
---
+++ Application/crud.py
@@ -0,0 +1,88 @@
+from flask import *  
+import sqlite3
+
+app = Flask(__name__)  
+ 
+@app.route("/")  
+def index():  
+    return render_template("index.html");  
+ 
+@app.route("/add")  
+def add():  
+    return render_template("add.html")  
+ 
+@app.route("/savedetails",methods = ["POST","GET"])  
+def saveDetails():  
+    msg = "msg"  
+    if request.method == "POST":  
+        try:  
+            name = request.form["name"]  
+            email = request.form["email"]  
+            address = request.form["address"]  
+            with sqlite3.connect("Student.db") as con:  
+                cur = con.cursor()  
+                cur.execute("INSERT into students (name, email, address) values (?,?,?)",(name,email,address))  
+                con.commit()  
+                msg = "Student successfully Added"  
+        except:  
+            con.rollback()  
+            msg = "We can not add the student to the list"  
+        finally:  
+            return render_template("success.html",msg = msg)  
+            con.close()
+
+@app.route("/update")  
+def update():  
+    return render_template("update.html")
+
+@app.route("/updatedetails",methods = ["POST","GET"])  
+def updateDetails():  
+    msg = "msg"  
+    if request.method == "POST":  
+        try:  
+            id = request.form["id"]  
+            name = request.form["name"]
+            email = request.form["email"]  
+            address = request.form["address"]  
+            with sqlite3.connect("Student.db") as con:  
+                cur = con.cursor()  
+                cur.execute("UPDATE students set  name=?, email=?,address=? where id=?",(name,email,address,id))  
+                con.commit()  
+                msg = "Student successfully Updated"  
+        except:  
+            con.rollback()  
+            msg = "We can not add the students to the list"  
+        finally:  
+            return render_template("success.html",msg = msg)  
+            con.close()   
+ 
+
+@app.route("/view")  
+def view():  
+    con = sqlite3.connect("Student.db")  
+    con.row_factory = sqlite3.Row  
+    cur = con.cursor()  
+    cur.execute("select * from students")  
+    rows = cur.fetchall()  
+    return render_template("view.html",rows = rows)  
+ 
+@app.route("/delete")  
+def delete():  
+    return render_template("delete.html")  
+ 
+@app.route("/deleterecord",methods = ["POST"])  
+def deleterecord():  
+    id = request.form["id"]  
+    with sqlite3.connect("Student.db") as con:  
+        try:
+            cur = con.cursor()  
+            cur.execute("delete from students where id = ?",id)  
+            msg = "record successfully deleted"  
+        except:  
+            msg = "can't be deleted"  
+        finally:  
+            return render_template("delete_record.html",msg = msg)  
+  
+if __name__ == "__main__":  
+    app.run(debug = True)
+    #app.run(host ='0.0.0.0', port = 5000, debug = True)  
